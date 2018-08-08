8c705e5aa3b7c31e34d98210f222268de44826db
chrissetiana <chrissetiana@gmail.com>
2018-08-08 08:08:05 +0100

    Initial commit

---
+++ app/src/main/java/com/chrissetiana/intentsified/MainActivity.java
@@ -0,0 +1,13 @@
+package com.chrissetiana.intentsified;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
