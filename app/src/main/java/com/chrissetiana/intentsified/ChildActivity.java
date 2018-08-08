4db7d4002d611254813c9681b288036e5b5def09
chrissetiana <chrissetiana@gmail.com>
2018-08-08 08:31:13 +0100

    Create another layout

---
+++ app/src/main/java/com/chrissetiana/intentsified/ChildActivity.java
@@ -0,0 +1,19 @@
+package com.chrissetiana.intentsified;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.TextView;
+
+public class ChildActivity extends Activity {
+
+    private TextView displayText;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        displayText = findViewById(R.id.text_display);
+    }
+}
