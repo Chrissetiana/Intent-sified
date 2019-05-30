770b3494337b7a6bdbbc478cec29d61a25adf1c5
Tiana Aiyemo <chrissetiana@gmail.com>
2019-05-30 23:17:42 +0100

    Add intent (implicit) (camera)

--- app/src/main/java/com/chrissetiana/intentsified/ChildActivity.java
+++ app/src/main/java/com/chrissetiana/intentsified/ChildActivity.java
@@ -1,19 +1,19 @@
 package com.chrissetiana.intentsified;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class ChildActivity extends Activity {
+public class ChildActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
 
-        TextView displayText = findViewById(R.id.text_display);
+        TextView displayText = findViewById(R.id.text_result);
 
         Intent intent = getIntent();
         if (intent.hasExtra(Intent.EXTRA_TEXT)) {
--- app/src/main/java/com/chrissetiana/intentsified/MainActivity.java
+++ app/src/main/java/com/chrissetiana/intentsified/MainActivity.java
@@ -2,36 +2,44 @@ package com.chrissetiana.intentsified;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText textView;
-    Button button;
+    public static final int REQUEST_CODE = 1;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private ImageView imageView;
+    private EditText textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.entry_text);
-        button = findViewById(R.id.button_entry);
+        textView = findViewById(R.id.text_entry);
+        Button button = findViewById(R.id.button_entry);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String text = textView.getText().toString().trim();
+
                 Context context = MainActivity.this;
                 Class destination = ChildActivity.class;
+
                 Intent intent = new Intent(context, destination);
                 intent.putExtra(Intent.EXTRA_TEXT, text);
                 startActivity(intent);
@@ -44,11 +52,12 @@ public class MainActivity extends AppCompatActivity {
         Uri uri = Uri.parse(url);
 
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
 
-        Log.d("MainActivity", uri.toString());
+        Log.d(LOG_TAG, uri.toString());
     }
 
     public void onClickOpenAddressButton(View view) {
@@ -61,11 +70,12 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(uri);
+
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
 
-        Log.d("MainActivity", uri.toString());
+        Log.d(LOG_TAG, uri.toString());
     }
 
     public void onClickShareTextButton(View view) {
@@ -80,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
                 .setText(text)
                 .startChooser();
 
-        Log.d("MainActivity", "\nFrom: " + this + "\nType: " + type + "\nMessage: " + text);
+        Log.d(LOG_TAG, "\nFrom: " + this + "\nType: " + type + "\nMessage: " + text);
     }
 
     public void createYourOwn(View view) {
@@ -93,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
 
         Calendar endTime = Calendar.getInstance();
         beginTime.set(2018, 11, 01, 12, 00, 00);
-        long end = beginTime.getTimeInMillis();
+        long end = endTime.getTimeInMillis();
 
         Intent intent = new Intent(Intent.ACTION_INSERT)
                 .setData(CalendarContract.Events.CONTENT_URI)
@@ -101,10 +111,30 @@ public class MainActivity extends AppCompatActivity {
                 .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
                 .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, begin)
                 .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, end);
+
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
 
-        Log.d("MainActivity", intent.toString());
+        Log.d(LOG_TAG, intent.toString());
+    }
+
+    public void onClickTakePicture(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 1);
+        }
+
+        Log.d(LOG_TAG, "Camera launched");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            Bitmap bitmap = (Bitmap) bundle.get("data");
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
